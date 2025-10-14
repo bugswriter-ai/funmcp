078820a099737bb9e1dd1cf6adb3e615eea6dd1d
Suraj Kushwah <suraj@bugswriter.com>
2025-10-14 22:40:53 +0530

    adding those server changes

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 setsid uv run fastmcp run servers/old_image_reviver.py:mcp --transport http --port 9001 &
 setsid uv run fastmcp run servers/grayscale_server.py:mcp --transport http --port 9002 &
-setsid uv run fastmcp run servers/upscaler.py:mcp --transport http --port 9003 &
+setsid uv run fastmcp run servers/ai-upscale.py:mcp --transport http --port 9003 &
