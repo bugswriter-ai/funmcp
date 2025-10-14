30f01a2635a89729a58e34b34b2bee80a32479f0
Jasmin <jasmin@oneplay.in>
2025-10-14 14:13:58 +0530

    Added Product banner creation MCP server.

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ setsid uv run fastmcp run servers/old_image_reviver.py:mcp --transport http --po
 setsid uv run fastmcp run servers/grayscale_server.py:mcp --transport http --port 9002 &
 setsid uv run fastmcp run servers/upscaler.py:mcp --transport http --port 9003 &
 setsid uv run fastmcp run servers/audio_clone_server.py:mcp --transport http --port 9004 &
+setsid uv run fastmcp run servers/product_photoshoot_server.py:mcp --transport http --port 9005 &
